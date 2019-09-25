764f4a7e207ac24f734cb162937b2bdc683f1a81
Stanislav Frolov <frolosofsky@gmail.com>
2019-09-25 23:38:30 +0200

    Buffering twice - brillian idea

--- main.cpp
+++ main.cpp
@@ -65,39 +65,18 @@ enum class MineWorkerResult {
     FINISHED,
 };
 
-// how many characters on the tail to process by single SHA1_Update.
-constexpr size_t update_threshold = 4;
-
-MineWorkerResult mine_worker(std::vector<SHA_CTX> &ctxs, unsigned char nonce[nonce_size], unsigned char hash[20], size_t difficulty) {
+MineWorkerResult mine_worker(SHA_CTX const &prefix_ctx, unsigned char nonce[nonce_size], unsigned char hash[20], size_t difficulty) {
     for (size_t i = 0; i < 9999999; ++i) {
         size_t changed_index = nonce_inc(nonce);
         if (changed_index == npos) {
             return MineWorkerResult::FINISHED;
         }
-        for (size_t j = changed_index; j <= nonce_size - update_threshold; ++j) {
-            ctxs[j + 1] = ctxs[j];
-            SHA1_Update(&ctxs[j + 1], nonce + j, 1);
-        }
-        ctxs[nonce_size - update_threshold + 1] = ctxs[nonce_size - update_threshold];
-        SHA1_Update(&ctxs[nonce_size - update_threshold + 1], nonce + nonce_size - update_threshold, update_threshold);
-        SHA1_Final(hash, &ctxs[nonce_size - update_threshold + 1]);
+        SHA_CTX ctx = prefix_ctx;
+        SHA1_Update(&ctx, nonce, nonce_size);
+        SHA1_Final(hash, &ctx);
         if (difficulty_eq(hash, difficulty)) {
             return MineWorkerResult::FOUND;
         }
-
-        // A version with no ctx cache is little bit slower (1-2s) for short nonces (4-5 characters)
-        // and significantly slower for longer nonces.
-
-        // size_t changed_index = nonce_inc(nonce);
-        // if (changed_index == npos) {
-        //     return MineWorkerResult::FINISHED;
-        // }
-        // SHA_CTX ctx = ctxs[0];
-        // SHA1_Update(&ctx, nonce, nonce_size);
-        // SHA1_Final(hash, &ctx);
-        // if (difficulty_eq(hash, difficulty)) {
-        //     return MineWorkerResult::FOUND;
-        // }
     }
     return MineWorkerResult::NOTFOUND;
 }
@@ -114,24 +93,9 @@ std::mutex m;
 std::condition_variable thread_exit;;
 
 void miner_thread(SHA_CTX const &prefix_ctx, MineResult &result, size_t difficulty, int id) {
-    // a ctx cache
-    // last `update_threshold` objects are effectively unused and present here for simplicity
-    //
-    // 0: prefix_ctx
-    // 1: ctx for nonce[0]
-    // ...
-    // nonce_size: ctx for nonce[-2]
-    // nonce_size+1: ctx for nonce[-1]
-    std::vector<SHA_CTX> ctxs(nonce_size + 1);
-    ctxs[0] = prefix_ctx;
-    for (size_t i = 1; i < ctxs.size(); ++i) {
-        ctxs[i] = ctxs[i - 1];
-        SHA1_Update(&ctxs[i], result.nonce + i - 1, 1);
-    }
-
     MineWorkerResult r = MineWorkerResult::NOTFOUND;
     while(result_id == -1 && r == MineWorkerResult::NOTFOUND) {
-        r = mine_worker(ctxs, result.nonce, result.hash, difficulty);
+        r = mine_worker(prefix_ctx, result.nonce, result.hash, difficulty);
     }
     if (r == MineWorkerResult::FOUND) {
         result_id = id;
@@ -198,7 +162,7 @@ std::string to_string(unsigned char const data[N]) {
 
 int main() {
     std::string const prefix = "hello world";
-    const auto r = mine(prefix, 8, 2);
+    const auto r = mine(prefix, 7, 2);
     if (!r.success) {
         std::cerr << "Hash not found" << std::endl;
         return 1;
