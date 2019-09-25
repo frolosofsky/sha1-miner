6814628c4951bb12dde64a1f4546be82cef2930b
Stanislav Frolov <frolosofsky@gmail.com>
2019-09-25 22:58:07 +0200

    Add some optimizations

--- main.cpp
+++ main.cpp
@@ -37,27 +37,26 @@ bool difficulty_eq(SHA_CTX const &ctx, unsigned char hash[20], size_t difficulty
     return difficulty_eq(hash, difficulty);
 }
 
-bool nonce_inc(unsigned char *buf, size_t size, size_t *buf_length, size_t start=0) {
-    // alphabet: 0-9a-zA-Z
-    if (start >= size - 1) {
-        return false;
+constexpr size_t npos = size_t(-1);
+constexpr size_t nonce_size = 16;
+
+// Nonce alphabet is [0-9a-zA-Z]
+size_t nonce_inc(unsigned char nonce[nonce_size], size_t i=(nonce_size - 1)) {
+    if (nonce[i] == '9') {
+        nonce[i] = 'a';
+        return i;
+    } else if (nonce[i] == 'z') {
+        nonce[i] = 'A';
+        return i;
+    } else if (nonce[i] != 'Z') {
+        ++nonce[i];
+        return i;
     }
-    if (buf[start] == 0) {
-        buf[start] = '0';
-        ++(*buf_length);
-        return true;
+    nonce[i] = '0';
+    if (i == 0) {
+        return npos;
     }
-    if (buf[start] == '9') {
-        buf[start] = 'a';
-    } else if (buf[start] == 'z') {
-        buf[start] = 'A';
-    } else if (buf[start] == 'Z') {
-        buf[start] = '0';
-        return nonce_inc(buf, size, buf_length, start + 1);
-    } else {
-        ++buf[start];
-    }
-    return true;
+    return nonce_inc(nonce, i - 1);
 }
 
 enum class MineWorkerResult {
@@ -66,18 +65,39 @@ enum class MineWorkerResult {
     FINISHED,
 };
 
-MineWorkerResult mine_worker(SHA_CTX const &prefix_ctx, unsigned char nonce[64], size_t *nonce_len, unsigned char hash[20], size_t difficulty) {
-    SHA_CTX ctx;
+// how many characters on the tail to process by single SHA1_Update.
+constexpr size_t update_threshold = 4;
+
+MineWorkerResult mine_worker(std::vector<SHA_CTX> &ctxs, unsigned char nonce[nonce_size], unsigned char hash[20], size_t difficulty) {
     for (size_t i = 0; i < 9999999; ++i) {
-        if (!nonce_inc(nonce, 64, nonce_len)) {
+        size_t changed_index = nonce_inc(nonce);
+        if (changed_index == npos) {
             return MineWorkerResult::FINISHED;
         }
-        memcpy(&ctx, &prefix_ctx, sizeof(ctx));
-        SHA1_Update(&ctx, nonce, *nonce_len);
-        SHA1_Final(hash, &ctx);
+        for (size_t j = changed_index; j <= nonce_size - update_threshold; ++j) {
+            ctxs[j + 1] = ctxs[j];
+            SHA1_Update(&ctxs[j + 1], nonce + j, 1);
+        }
+        ctxs[nonce_size - update_threshold + 1] = ctxs[nonce_size - update_threshold];
+        SHA1_Update(&ctxs[nonce_size - update_threshold + 1], nonce + nonce_size - update_threshold, update_threshold);
+        SHA1_Final(hash, &ctxs[nonce_size - update_threshold + 1]);
         if (difficulty_eq(hash, difficulty)) {
             return MineWorkerResult::FOUND;
         }
+
+        // A version with no ctx cache is little bit slower (1-2s) for short nonces (4-5 characters)
+        // and significantly slower for longer nonces.
+
+        // size_t changed_index = nonce_inc(nonce);
+        // if (changed_index == npos) {
+        //     return MineWorkerResult::FINISHED;
+        // }
+        // SHA_CTX ctx = ctxs[0];
+        // SHA1_Update(&ctx, nonce, nonce_size);
+        // SHA1_Final(hash, &ctx);
+        // if (difficulty_eq(hash, difficulty)) {
+        //     return MineWorkerResult::FOUND;
+        // }
     }
     return MineWorkerResult::NOTFOUND;
 }
@@ -85,7 +105,7 @@ MineWorkerResult mine_worker(SHA_CTX const &prefix_ctx, unsigned char nonce[64],
 struct MineResult {
     bool success = false;
     unsigned char padding[64] = {0};
-    unsigned char nonce[64] = {0};
+    unsigned char nonce[nonce_size] = {0};
     unsigned char hash[20] = {};
 };
 
@@ -94,10 +114,24 @@ std::mutex m;
 std::condition_variable thread_exit;;
 
 void miner_thread(SHA_CTX const &prefix_ctx, MineResult &result, size_t difficulty, int id) {
-    size_t nonce_len;
+    // a ctx cache
+    // last `update_threshold` objects are effectively unused and present here for simplicity
+    //
+    // 0: prefix_ctx
+    // 1: ctx for nonce[0]
+    // ...
+    // nonce_size: ctx for nonce[-2]
+    // nonce_size+1: ctx for nonce[-1]
+    std::vector<SHA_CTX> ctxs(nonce_size + 1);
+    ctxs[0] = prefix_ctx;
+    for (size_t i = 1; i < ctxs.size(); ++i) {
+        ctxs[i] = ctxs[i - 1];
+        SHA1_Update(&ctxs[i], result.nonce + i - 1, 1);
+    }
+
     MineWorkerResult r = MineWorkerResult::NOTFOUND;
     while(result_id == -1 && r == MineWorkerResult::NOTFOUND) {
-        r = mine_worker(prefix_ctx, result.nonce, &nonce_len, result.hash, difficulty);
+        r = mine_worker(ctxs, result.nonce, result.hash, difficulty);
     }
     if (r == MineWorkerResult::FOUND) {
         result_id = id;
@@ -129,6 +163,7 @@ MineResult mine(std::string const &prefix, size_t difficulty, size_t threads_cou
     const size_t padding_size = 64 - prefix.size() % 64;
     for (size_t i = 0; i < threads_count; ++i) {
         memset(results[i].padding, padding_alphabet[i], padding_size);
+        memset(results[i].nonce, '0', nonce_size);
         SHA1_Update(&ctxs[i], results[i].padding, padding_size);
         if (difficulty_eq(ctxs[i], results[i].hash, difficulty)) {
             return results[i];
@@ -156,18 +191,19 @@ MineResult mine(std::string const &prefix, size_t difficulty, size_t threads_cou
     return result;
 }
 
-std::string to_string(unsigned char const data[64]) {
-    return std::string((char const*)data, 64);
+template<size_t N>
+std::string to_string(unsigned char const data[N]) {
+    return std::string((char const*)data, N);
 }
 
 int main() {
     std::string const prefix = "hello world";
-    const auto r = mine(prefix, 9, 2);
+    const auto r = mine(prefix, 8, 2);
     if (!r.success) {
         std::cerr << "Hash not found" << std::endl;
         return 1;
     }
-    std::cout << prefix << to_string(r.padding) << to_string(r.nonce) << std::endl;
+    std::cout << prefix << to_string<64>(r.padding) << to_string<nonce_size>(r.nonce) << std::endl;
     std::cout << hash2str(r.hash, sizeof(r.hash)) << std::endl;
     return 0;
 }
